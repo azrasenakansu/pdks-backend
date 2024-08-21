49e23b895c54ea400717bbe2da7b5f7f49e2e169
azrasenakansu <104143591+azrasenakansu@users.noreply.github.com>
2024-08-21 13:26:04 +0300

    initial commit

--- src/main/java/com/proven/pdks/services/AuthanticationServiceImpl.java
+++ src/main/java/com/proven/pdks/services/AuthanticationServiceImpl.java
@@ -5,12 +5,9 @@ import com.proven.pdks.dtos.LoginResponseDto;
 import com.proven.pdks.entities.User;
 import com.proven.pdks.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class AuthanticationServiceImpl implements AuthanticationService {
