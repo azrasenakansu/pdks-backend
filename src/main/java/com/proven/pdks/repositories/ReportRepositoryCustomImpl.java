94cf1afbcfd178beb77057a59fb57b5a7ffd3921
azrasenakansu <104143591+azrasenakansu@users.noreply.github.com>
2024-08-27 14:50:27 +0300

    add where is_approved=true filtering

--- src/main/java/com/proven/pdks/repositories/ReportRepositoryCustomImpl.java
+++ src/main/java/com/proven/pdks/repositories/ReportRepositoryCustomImpl.java
@@ -23,7 +23,7 @@ public class ReportRepositoryCustomImpl implements ReportRepository {
     @Override
     public List<WorklogReportDTO> getReport(LocalDate startDate, LocalDate endDate, List<String> tckns) {
         String sql = "SELECT users.name,report.* FROM users INNER JOIN (SELECT COALESCE(wrk.tckn,ext.tckn) as tckn, COALESCE(wrk.date,ext.date) as date, wrk.start_time, wrk.end_time, ext.ext_hours, ext.ext_descriptions FROM worklogs AS wrk FULL JOIN " +
-                "(SELECT date,tckn, SUM(end_time-start_time) as ext_hours , STRING_AGG(description,'-$-') as ext_descriptions FROM external_worklogs GROUP BY (date,tckn)) AS ext " +
+                "(SELECT date,tckn, SUM(end_time-start_time) as ext_hours , STRING_AGG(description,'-$-') as ext_descriptions FROM external_worklogs WHERE is_approved = TRUE GROUP BY (date,tckn)) AS ext " +
                 "ON ext.tckn = wrk.tckn AND ext.date = wrk.date) AS report ON report.tckn = users.tckn WHERE report.date BETWEEN ? AND ? AND ( ? OR report.tckn IN (?) )";
         return jdbcTemplate.query(sql, new Object[]{startDate, endDate, tckns.isEmpty(), StringUtils.join(tckns, ',') }, new BeanPropertyRowMapper<>(WorklogReportDTO.class));
     }
