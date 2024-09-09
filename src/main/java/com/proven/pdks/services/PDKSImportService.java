d8577cd3d7bbdb2a6eb4db615db275cf022a0898
Burak Erdinç <berdinc@provenbilisim.com>
2024-09-09 17:29:28 +0300

    new pdks import logic
    logs will be removed later.

--- src/main/java/com/proven/pdks/services/PDKSImportService.java
+++ src/main/java/com/proven/pdks/services/PDKSImportService.java
@@ -6,39 +6,53 @@ import com.proven.pdks.entities.Worklog;
 import com.proven.pdks.repositories.UserRepository;
 import com.proven.pdks.repositories.WorklogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class PDKSImportService {
+    @Autowired
+    private final JdbcTemplate jdbcTemplate;
     @Autowired
     private final UserRepository userRepository;
     @Autowired
     private final WorklogRepository worklogRepository;
 
-    public PDKSImportService(UserRepository userRepository, WorklogRepository worklogRepository) {
+    public PDKSImportService(JdbcTemplate jdbcTemplate, UserRepository userRepository, WorklogRepository worklogRepository) {
+        this.jdbcTemplate = jdbcTemplate;
         this.userRepository = userRepository;
         this.worklogRepository = worklogRepository;
     }
 
-    public void importPDKS(List<SimpleRows> rows) {
-        LocalDate lastImportDate = worklogRepository.findTopByOrderByDateDesc().map(Worklog::getDate).orElse(null);
+    private Map<String, LocalDate> userLastImportMap(){
+        Map<String, LocalDate> map = new HashMap<>();
+        String sql = "SELECT users.tckn, wg.lastImport FROM users LEFT JOIN ( SELECT tckn, MAX(date) AS lastImport FROM worklogs GROUP BY tckn ) wg ON users.tckn = wg.tckn";
+        jdbcTemplate.query(sql, (row) -> {
+            String tckn = row.getString(1);
+            Date date = row.getDate(2);
+            map.put(tckn, date != null ? date.toLocalDate() : null);
+        });
+        return map;
+    }
+
+    public void importPDKS(List<SimpleRows> rows){
+        Map<String, LocalDate> importMap = userLastImportMap();
         LocalDate now = LocalDate.now();
-        rows = rows.stream().filter(row -> row.getDate().isBefore(now) && (lastImportDate == null || row.getDate().isAfter(lastImportDate))).toList();
+        System.err.println("Raw data count: " + rows.size());
+        rows = rows.stream().filter(row -> importMap.containsKey(row.getTckn()) &&
+                row.getDate().isBefore(now) && (importMap.get(row.getTckn()) == null || row.getDate().isAfter(importMap.get(row.getTckn())))).toList();
+        System.err.println("Filtered data count: " + rows.size());
 
         Map<String, Map<LocalDate, Worklog>> result = new HashMap<>();
-
         for (SimpleRows row : rows) {
-            User user = userRepository.findByTckn(row.getTckn());
-
-            if (user == null) {
-                continue;
-            }
-
             if (!result.containsKey(row.getTckn())) {
                 result.put(row.getTckn(), new HashMap<>());
             }
@@ -46,7 +60,7 @@ public class PDKSImportService {
             Map<LocalDate, Worklog> dateLogs = result.get(row.getTckn());
             if (!dateLogs.containsKey(row.getDate())) {
                 Worklog temp = new Worklog();
-                temp.setUser(user);
+                temp.setUser(userRepository.findByTckn(row.getTckn()));
                 temp.setDate(row.getDate());
                 dateLogs.put(row.getDate(), temp);
             }
@@ -64,5 +78,7 @@ public class PDKSImportService {
         }
         List<Worklog> worklogs = result.values().stream().flatMap(q -> q.values().stream()).toList();
         worklogRepository.saveAllAndFlush(worklogs);
+        System.err.println("Imported worklog count: " + worklogs.size());
     }
+
 }
